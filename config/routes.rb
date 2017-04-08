e5db8b2fdf1c9d50e4809ab5debf7c713999830a
Andrew <andrewsouthard111@gmail.com>
2017-04-08 11:13:00 -0400

    Add password login/signup authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/signup" => "users#new"
-  post "/users" => "users#create"
+  get 'posts/index'
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'posts#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
